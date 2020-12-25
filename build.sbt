c954ab0fbacbccbc00eaf27dbedaf12517b57a18
beccosauro <cerioli.simone@gmail.com>
2020-12-25 22:14:20 +0100

    now the repo create an uber jan and is runned with docker -run. Can write the file on HDFS. easy to add to the crontab

--- build.sbt
+++ build.sbt
@@ -1,9 +1,14 @@
-name := "Uni-afbd-last-Fm"
 
-version := "0.1"
-
+lazy val commonSettings = Seq(
+  version := "0.1-SNAPSHOT",
+  organization := "com.example",
+  scalaVersion := "2.10.1",
+  test in assembly := {}
+)
 scalaVersion := "2.11.8"
+
 val sparkVersion = "2.1.0"
+
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion,
--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,14 +1,11 @@
 package app
 
 import de.umass.lastfm.Caller
-import entities.User
 import lastFm.Client
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import traits.SparkSessionWrapper
 
-object Main extends SparkSessionWrapper {
-  def main(args: Array[String]): Unit = {
+object Main extends SparkSessionWrapper with App {
     Caller.getInstance.setUserAgent("test-beccosauro")
     val client  = new Client("dbf6b9f99ea1ee2762fcde6c1f17baff")
     //val topHits = spark.createDataFrame(spark.sparkContext.parallelize(client.getTopHits(100).toList))
@@ -18,5 +15,5 @@ object Main extends SparkSessionWrapper {
     df.show(1000,false)
     df.repartition(1).write.option("header","true").mode(saveMode = "overwrite").parquet("output/test")
 
-  }
+
 }

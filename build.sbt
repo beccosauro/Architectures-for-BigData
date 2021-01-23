894cb467b99a0a2dfbb9055cbd68e2fdb7b30d41
beccosauro <cerioli.simone@gmail.com>
2021-01-23 20:38:33 +0100

    fining tuning. adding recovery log in case of failure

--- build.sbt
+++ build.sbt
@@ -7,9 +7,8 @@ val sparkVersion = "2.1.0"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion,
-  "de.u-mass" % "lastfm-java" % "0.1.2"
-
-)
+  "de.u-mass" % "lastfm-java" % "0.1.2",
+  "org.scalatest" %% "scalatest" % "3.1.1" % "test")
 assemblyJarName in assembly := "lastFm-ETL-app.jar"
 assemblyMergeStrategy in assembly := {
   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
--- src/main/scala/app/ETLIngestion.scala
+++ src/main/scala/app/ETLIngestion.scala
@@ -7,18 +7,18 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Row, SparkSession}
-import utilities.util
+import utilities.util._
 object ETLIngestion {
   def main(args: Array[String]) {
     Caller.getInstance.setUserAgent("test-beccosauro")
     val client = new Client("dbf6b9f99ea1ee2762fcde6c1f17baff")
     val conf = new SparkConf().setMaster("spark://spark-master:7077").setAppName("SparkWriteApplication")
-    val spark = SparkSession.builder().config(conf).getOrCreate()
-    import spark.implicits._
 
-    val logger: Logger = Logger.getLogger("INGESTION ETL")
-    val userData = client.populate(2, limit = 500, from = util.getMidnightYesterday, to = util.getMidnightToday)
-    logger.info("Numero canzoni da aggiungere" + userData.size)
+    implicit val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+
+    import spark.implicits._
+    val last_ingest = "/ingestion_state"
+    val userData = client.populate(2, limit = 500, from = getEpochLastIngestion(last_ingest), to = getMidnightToday)
     val userDataDf = (spark createDataFrame spark.sparkContext.parallelize(userData))
       .withColumn("date", to_date(from_unixtime(col("startSong"))))
       .withColumn("year", year(col("date")))
@@ -30,16 +30,16 @@ object ETLIngestion {
       .select("artist", "title")
       .distinct()
       .withColumn("index", row_number().over(Window.orderBy("artist", "title")))
-      .repartition(5)
+      .repartition(10)
       .cache()
     val totSong = spark.sparkContext.broadcast(allTrack.count())
-    totSong.value
+
     val updatedTrack = allTrack.rdd.mapPartitions { iter =>
       val logger: Logger = Logger.getLogger("INGESTION ETL")
       iter.map { r: Row =>
         val c = new Client("dbf6b9f99ea1ee2762fcde6c1f17baff")
         logger.info("song processed: " + r.getInt(2) + " of: " + totSong.value)
-        Thread.sleep(100)
+        Thread.sleep(270)
         c.getInfoTrack(r.getString(0), r.getString(1))
       }
     }.toDF("title", "artist", "genre", "duration")
@@ -60,6 +60,6 @@ object ETLIngestion {
       .parquet("/data/raw/track")
 
     Logger.getLogger("INGESTION ETL").info("PROCESSO INGESTION CONCLUSO CORRETTAMENTE")
-
+    writeLastIngestion(last_ingest)
   }
 }
